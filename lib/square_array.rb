3414aab327ec812d027a2fc315d7d2378ba6a76f
Patricia Arnedo <paaarnedo@gmail.com>
2020-09-21 23:39:40 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,12 +3,9 @@ def square_array(array)
   new_array = []
   
   while array[count] do
-    new_array.push(array[count]**)
-    
-    
+    new_array.push(array[count]**2)
+    count += 1
   end
     
-    
-  
-  
+    new_array
 end
